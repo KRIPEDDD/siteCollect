28cee7fdfef4723c3601711adb528efea6db5a62
liweile <1073967816@qq.com>
2024-08-20 22:41:43 +0800

    1

---
+++ _nuxt/YKcfMsOw.js
@@ -0,0 +1,1 @@
+import{S as i,r as a}from"./DY2KoslE.js";const r=i("asideStore",()=>{const s=a(""),t=a({});return{asideSelIndex:s,asideDatalist:t,setAsideSelIndex:e=>{s.value=e},setAsideDatalist:e=>{t.value=e}}});export{r as u};
